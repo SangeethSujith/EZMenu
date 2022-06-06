2e01ad05492bb1471f7d3f75c992d3d76fe07c76
Sangeeth PS <sangeethps2255@gmail.com>
2022-06-06 16:50:02 +0530

    Added a comment

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Seats from './assets/screens/Seats';
 import Bookfood from './assets/screens/Bookfood';
 
 const Stack = createNativeStackNavigator();
-
+//this is a test
 const App = () => {
   return (
     <NavigationContainer>
