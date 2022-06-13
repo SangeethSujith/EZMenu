5739f0e074fed6154e065343ca12ecf4df4062f9
Sangeeth PS <sangeethps2255@gmail.com>
2022-06-13 14:04:16 +0530

    Added some feature and ui elements to login

--- assets/screens/Login.js
+++ assets/screens/Login.js
@@ -1,44 +1,55 @@
 /* eslint-disable prettier/prettier */
-import {Text, View, StatusBar, SafeAreaView, TextInput, TouchableOpacity} from 'react-native';
-import React from 'react';
+import { Text, View, StatusBar, SafeAreaView, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
 import Design from '../styles/Design';
-const Login = ({navigation}) => {
-  return (
-    <SafeAreaView style={Design.container}>
-      <StatusBar
-        barStyle="dark-content"
-        translucent={true}
-        backgroundColor={'transparent'}
-      />
-      <View style={Design.tophalf}>
-        <Text style={Design.h1}>Sign In</Text>
-        <Text style={Design.h2}>Enter your quick access pin</Text>
-        <TextInput
-          style={Design.input}
-          placeholder="* * * *"
-          keyboardType="numeric"
-          placeholderTextColor="rgba(130,139,154,0.5)"
-        />
-      </View>
-      <View><Text style={Design.or}>OR</Text></View>
-      <View style={Design.bottomhalf}>
-        <Text style={Design.h2}>User name</Text>
-        <TextInput style={Design.input}
-        placeholder="User name"
-        placeholderTextColor="rgba(130,139,154,0.5)"
-        />
-         <Text style={Design.h2}>Password</Text>
-        <TextInput style={Design.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        placeholderTextColor="rgba(130,139,154,0.5)"
-        />
-        <TouchableOpacity style={Design.btn} onPress={() => navigation.navigate('Seats')}>
-            <Text style={Design.btntxt}>Login</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
+const Login = ({ navigation }) => {
+    const [visible, setvisible] = useState(true);
+    return (
+        <SafeAreaView style={Design.container}>
+            <StatusBar
+                barStyle="dark-content"
+                translucent={true}
+                backgroundColor={'transparent'}
+            />
+            <View style={Design.tophalf}>
+                <Text style={Design.h1}>Sign In</Text>
+                <Text style={Design.h2}>Enter your quick access pin</Text>
+                <TextInput
+                    style={Design.input}
+                    placeholder="* * * *"
+                    keyboardType="numeric"
+                    placeholderTextColor="rgba(130,139,154,0.5)"
+                />
+            </View>
+            <View><Text style={Design.or}>OR</Text></View>
+            <View style={Design.bottomhalf}>
+                <Text style={Design.h2}>User name</Text>
+                <View style={Design.inputv}>
+                    <TextInput style={Design.inpute}
+                        placeholder="User name"
+                        placeholderTextColor="rgba(130,139,154,0.5)"
+                    />
+                    <Image source={require('../images/tick.png')}/>
+                    </View>
+                <Text style={Design.h2}>Password</Text>
+                <View style={Design.inputv}>
+                    <TextInput style={Design.inpute}
+                        placeholder="Password"
+                        secureTextEntry={visible}
+                        placeholderTextColor="rgba(130,139,154,0.5)"
+                    />
+                    <TouchableOpacity onPress={() => (
+                        setvisible(prev => !prev)
+                    )}>
+                        <Image source={require('../images/eye.png')} />
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={Design.btn} onPress={() => navigation.navigate('Seats')}>
+                    <Text style={Design.btntxt}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default Login;
